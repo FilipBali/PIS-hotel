5067c10da07566b90d5e2e0419d1dedb0fca2245
jofroger <36229889+jofroger@users.noreply.github.com>
2022-03-17 23:38:36 +0100

    add layouts

---
+++ src/frontend/layouts/chef.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app dark color="primary">
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn
+        v-for="(item, i) in items"
+        :key="i"
+        min-height="100%"
+        plain
+        :to="item.to"
+        ><b>{{ item.title }}</b></v-btn
+      >
+      <v-spacer />
+      <v-avatar color="secondary" size="40">JH</v-avatar>
+      <v-btn plain min-height="100%"> Janko Hrasko </v-btn>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon plain v-bind="attrs" v-on="on">
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </template>
+        <span>Odhlásiť sa</span>
+      </v-tooltip>
+    </v-app-bar>
+    <v-main>
+      <v-container fill-height>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app dark color="primary">
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "ReceptionistLayout",
+  data() {
+    return {
+      fixed: false,
+      title: "Hotel PIS",
+      items: [
+        {
+          title: "Stravovanie",
+          to: "/chef/meals",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
