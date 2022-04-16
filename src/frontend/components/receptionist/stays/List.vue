2c0add56d6a442e663efd25992aa8fd415c912e1
Filip Bali <xbalif00@stud.fit.vutbr.cz>
2022-04-16 13:48:59 +0200

    Fix receptionist\stays: form overflow

--- src/frontend/components/receptionist/stays/List.vue
+++ src/frontend/components/receptionist/stays/List.vue
@@ -40,13 +40,12 @@
 
       </v-data-table>
       <template>
-        <v-row justify="center">
           <v-dialog
             v-model="dialog"
             persistent
             max-width="600px"
           >
-                      <v-card>
+            <v-card>
               <v-card-title>
                 <span class="text-h5">Zmeniť pobyt</span>
               </v-card-title>
@@ -187,7 +186,6 @@
               </v-card-actions>
             </v-card>
           </v-dialog>
-        </v-row>
       </template>
     </v-card-text>
   </v-card>
